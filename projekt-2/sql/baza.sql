3b39ca1992946867fbcf4da6effbc1fedd5bca27
Aleksandra Kuś <63848081+awku@users.noreply.github.com>
2021-03-09 20:51:21 +0100

    Create baza.sql

---
+++ projekt-2/sql/baza.sql
@@ -0,0 +1,2 @@
+create table survey ( id serial primary key, dateandtime char(30), gender char(10), age integer, season char(10) ) ;
+create table user ( id serial primary key, email char(50), pswd char(50)) ;
